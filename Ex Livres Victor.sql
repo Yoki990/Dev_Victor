28b89b85ec98e6b4502b09cc50a12750a78fcfbb
Victor <victor@tang.com>
2026-01-13 09:56:25 +0100

    Exercices Livres Victor

---
+++ Ex Livres Victor.sql
@@ -0,0 +1,8 @@
+  CREATE TABLE IF NOT EXISTS Livres (
+	id_livre INT,
+    titre VARCHAR(255),
+    auteur VARCHAR(255),
+    annee_publication DATE,
+    genre VARCHAR(255),
+    exemplaires_disponibles INT
+)
